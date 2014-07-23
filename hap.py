fe95473efac85ee9decaae42eb27670ba2e5ffb1
Brett Sears <brettrsears@gmail.com>
2014-07-23 23:05:52 +0000

    Updating Hap.py to the more developed version.

--- hap.py
+++ hap.py
@@ -1,62 +1,59 @@
-#! /usr/bin/env python
-#Invoke this script as, well, a script. 'python hap.py' should do the trick.
-#Python string script
-#textboxstring.py
-#V0.2 - Getting the terminal down!
-import os
-import time
-import thread
-def input_thread(L):
-    raw_input()
-    L.append("yo")
-def handle(a):
-    os.system('clear')
-    print a + '\n'
-    question = raw_input("Would you like to do it again? y/n ")
-    while 1:    
-        os.system('clear')
-        print a + '\n'        
-        if question == "y":
-            user_input()
-        elif question == "n":
-            os.system('clear')
-            quit()
-        else:        
-            question = raw_input("Sorry, I didn't quite catch that." '\n' "Would you like to do it again? y/n ")
-def do_print(b):
-    L = []
-    counter = 1
-    thread.start_new_thread(input_thread, (L,))
-    while 1:
-        os.system('clear')
-	counter = counter + 1
-        if counter % 2 == 0:
-            print b + "_"
-        elif counter % 2 != 0:
-            print b
-        if L:  
-            handle(a = b)
-        print '\n' + "Press Enter to continue."
-        time.sleep(.1)
-def tprocess(c):
-    counter = 0   
-    storage = len(c)
-    wordinprogress = ""
-    while counter < storage + 1:
-        os.system('clear')
-        counter = counter + 1    
-        if counter % 4 != 0 and counter != storage + 1:
-            os.system('clear')
-            print wordinprogress
-        if counter % 4 == 0 and counter != storage + 1:
-            os.system('clear')
-            print wordinprogress + "_"
-        if counter == storage + 1:
-            do_print(b = c)
-        wordinprogress = c[:counter]
-        time.sleep(0.07)
+#!/usr/bin/env python
+import os				#needed for multi-platform compatibility
+import time				#needed to limit program speed
+import thread			#needed for the magical "close on enter"
+
+def input_thread(L):	#raw_input looks for enter in order to be like yooo that's my input
+	raw_input()
+	L.append(None)
+
+def handle_it():
+	question = raw_input('Would you like to use another string? (y|yes|Yes or n|no|No): ')
+	while 1:	
+		os.system('cls' if os.name == 'nt' else 'clear') 	
+		if question == "y" or question == "yes" or question == "Yes":
+			user_input()
+		elif question == "n" or question == "no" or question == "No":
+			print "Thanks for your time!"
+			quit()
+		else:
+			question = raw_input("I didn't quite catch that." '\n' "Would you like to use another string? (y|yes|Yes or n|no|No): ")
+def advance(b):			#what to do when current string is complete
+	timeout = 0			#decides when _ blinks
+	L = []				#magical array for closing program
+	thread.start_new_thread(input_thread, (L,)) #start input thread
+
+	while 1:			#loop for _
+		os.system('cls' if os.name == 'nt' else 'clear') #clear
+	 	timeout = timeout + 1	#counting variable
+		if timeout % 2 == 0:	#if odd, print normal
+			print b	
+		if timeout % 2 != 0:	#if even, blink _
+			print b + "_"	
+		print '\n', "Press Enter to continue." #always ran
+		time.sleep(0.5) 	#this is to give the blinking effect
+		if L: 		
+			os.system('cls' if os.name == 'nt' else 'clear') 
+			handle_it() 	
+
+def text_process(a):	#progressive text
+	wordinprogress = ""	#empty variable needs to be initialized
+	counter = 0			#counter variable
+	storage = len(a)	#length of observed string			
+	
+	while counter < storage + 1:	#while counting variable is less than the full variable
+		os.system('cls' if os.name == 'nt' else 'clear')	#clear screen
+		counter = counter + 1		#counting
+		if counter % 4 == 0 and counter != storage + 1:		#75% of the time, do not blink
+			print wordinprogress							
+		if counter % 4 != 0 and counter != storage + 1:		#25% of the time, blink _
+			print wordinprogress + "_"
+		if counter == storage + 1:	#prevents an overflow
+			print a
+			advance(b = a)	#hand off string to advance
+		wordinprogress = a[:counter] #progressively grow the string in progress to hold the entire string
+		time.sleep(0.07)	#slight pause
 def user_input():
-    os.system('clear')
-    string = raw_input("Please type desired text: ")
-    tprocess(c = string)
+	c = raw_input('Please type the text you would like to see advance: ')
+	text_process(a = c)
 user_input()
